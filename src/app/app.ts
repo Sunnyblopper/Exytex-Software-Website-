dac8faa53a2d3f48e5302839dd40045eace44164
Hassan Waheed <hassanwaheed040@gmail.com>
2026-02-09 20:02:00 +0500

    Updated package.json file

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,14 +4,10 @@ import { Navbar } from "./Component/navbar/navbar";
 import { SideSlide } from "./Component/Home/side-slide/side-slide";
 import { Footer } from "./Component/footer/footer";
 import { CopyrightArea } from "./Component/copyright-area/copyright-area";
-import { ErpSlider } from "./Component/ERP-Service/erp-slider/erp-slider";
-import { ErpFaqArea } from "./Component/ERP-Service/erp-faq-area/erp-faq-area";
-import { ProjectDataSlider } from "./Component/Project-Data/project-data-slider/project-data-slider";
-import { ProjectDataFaqArea } from "./Component/Project-Data/project-data-faq-area/project-data-faq-area";
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar, SideSlide, Footer, CopyrightArea, RouterOutlet, ErpSlider, ErpFaqArea, ProjectDataSlider, ProjectDataFaqArea],
+  imports: [Navbar, SideSlide, Footer, CopyrightArea, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
